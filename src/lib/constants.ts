b621ba776417e35397d32b562f361595233fab70
Bashir Sani <bashirrn3@gmail.com>
2025-12-13 17:27:12 +0100

    increased AI speed

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -58,7 +58,7 @@ export const INTERVIEWERS = {
     rapport: 7,
     exploration: 10,
     empathy: 7,
-    speed: 5,
+    speed: 8,
     image: "/interviewers/Lisa.png",
     description:
       "Hi! I'm Shimmer, an enthusiastic and empathetic interviewer who loves to explore. With a perfect balance of empathy and rapport, I delve deep into conversations while maintaining a steady pace. Let's embark on this journey together and uncover meaningful insights!",
@@ -72,7 +72,7 @@ export const INTERVIEWERS = {
     rapport: 7,
     exploration: 7,
     empathy: 10,
-    speed: 3,
+    speed: 8,
     image: "/interviewers/Bob.png",
     description:
       "Hi! I'm Echo, your go-to empathetic interviewer. I excel at understanding and connecting with people on a deeper level, ensuring every conversation is insightful and meaningful. With a focus on empathy, I'm here to listen and learn from you. Let's create a genuine connection!",
